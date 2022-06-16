e50ce09aff49769c63cc79eac5562ebfe6e15a4b
Stefan Schmidt <thrimbor.github@gmail.com>
2022-06-16 17:02:11 +0200

    Move ISO extraction out of main function

--- xbiso.cpp
+++ xbiso.cpp
@@ -1,6 +1,6 @@
 /*
  * xbiso
- * Copyright (C) 2015 Stefan Schmidt
+ * Copyright (C) 2015-2022 Stefan Schmidt
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,18 +21,10 @@
 #include <vector>
 #include <algorithm>
 #include "optionparser.h"
+#include "xbiso.hpp"
 #include <xbisoConfig.h>
 
-#if defined _WIN32
-    #include "direct.h"
-    #define mkdir(a,b) _mkdir(a)
-    #define chdir(a) _chdir(a)
-#else
-    #include <unistd.h>
-    #include <sys/stat.h>
-#endif
 
-void handleDirectoryEntry (std::ifstream& file, xdvdfs::DirectoryEntry& dirent);
 
 struct Arg: public option::Arg {
     static option::ArgStatus NonEmpty (const option::Option& option, bool msg) {
@@ -121,40 +113,7 @@ int main (int argc, char* argv[])
             std::string filename = parse.nonOption(i);
             std::string dirname = options[DIRECTORY] ? options[DIRECTORY].arg : filename.substr(0, filename.find_last_of("."));
 
-            std::cout << "extracting " << filename << " to " << dirname << std::endl;
-
-            std::ifstream isofile;
-            isofile.open(filename.c_str(), std::ifstream::binary | std::ifstream::in);
-            if (!isofile.is_open()) {
-                std::cerr << "ERROR: Could not open file '" << filename << "'" << std::endl;
-                return 1;
-            }
-
-            isofile.exceptions(std::ifstream::failbit | std::ifstream::badbit | std::ifstream::eofbit);
-
-            xdvdfs::VolumeDescriptor vd;
-            vd.readFromFile(isofile);
-
-            try {
-                vd.validate();
-            } catch (xdvdfs::Exception &e) {
-                // Try again with the sector offset for Redump files
-                vd.readFromFile(isofile, 0x30600);
-                vd.validate();
-                std::cout << "Redump-style ISO detected" << std::endl;
-            }
-            vd.validate();
-
-            if (!dryRun) {
-                mkdir(dirname.c_str(), 0755);
-                chdir(dirname.c_str());
-            }
-
-            xdvdfs::DirectoryEntry de = vd.getRootDirEntry(isofile);
-            handleDirectoryEntry(isofile, de);
-
-            if (!dryRun)
-                chdir("..");
+            extractISO(filename, dirname);
         }
     } else {
         printUsage();
@@ -163,49 +122,3 @@ int main (int argc, char* argv[])
     return 0;
 }
 
-void handleDirectoryEntry (std::ifstream& file, xdvdfs::DirectoryEntry& dirent)
-{
-    if (dirent.isDirectory())
-    {
-        std::cout << "creating directory " << dirent.getFilename() << std::endl;
-
-        if (!dryRun) {
-            mkdir(dirent.getFilename().c_str(), 0755);
-            chdir(dirent.getFilename().c_str());
-        }
-
-        xdvdfs::DirectoryEntry de = dirent.getFirstEntry(file);
-        handleDirectoryEntry(file, de);
-
-        if (!dryRun)
-            chdir("..");
-    }
-    else
-    {
-        std::cout << "extracting " << dirent.getFilename() << std::endl;
-
-        if (!dryRun)
-        {
-            std::ofstream efile;
-            efile.open(dirent.getFilename().c_str(), std::ofstream::out | std::ofstream::binary | std::ofstream::trunc);
-
-            if (!efile.is_open()) {
-                std::cerr << "failed to open file '" << dirent.getFilename() << "'" << std::endl;
-            }
-
-            dirent.extractFile(file, efile);
-        }
-    }
-
-    if (dirent.hasLeftChild())
-    {
-        xdvdfs::DirectoryEntry de = dirent.getLeftChild(file);
-        handleDirectoryEntry(file, de);
-    }
-
-    if (dirent.hasRightChild())
-    {
-        xdvdfs::DirectoryEntry de = dirent.getRightChild(file);
-        handleDirectoryEntry(file, de);
-    }
-}
